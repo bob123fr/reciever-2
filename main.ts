3319910e00faced9b9698276f14466a8b479cb98
bob123fr <100716851+bob123fr@users.noreply.github.com>
2022-04-02 18:07:27 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,35 @@
+radio.onReceivedString(function (receivedString) {
+    for (let index = 0; index < 4; index++) {
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 100)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 100)
+        basic.pause(1000)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 59)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, 59)
+        basic.pause(1000)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, 75)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Forward, 75)
+        basic.pause(1000)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Reverse, 27)
+        Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor2, Kitronik_Robotics_Board.MotorDirection.Reverse, 27)
+        basic.pause(5000)
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 90)
+        basic.pause(1000)
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 54)
+        basic.pause(1000)
+        Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 26)
+        basic.pause(100)
+    }
+    Kitronik_Robotics_Board.allOff()
+})
+radio.setGroup(1)
+Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 16)
+basic.showLeds(`
+    # # . # #
+    . . # # .
+    . # . # #
+    . # . . #
+    # . . . .
+    `)
+loops.everyInterval(1000, function () {
+    Kitronik_Robotics_Board.allOff()
+})
